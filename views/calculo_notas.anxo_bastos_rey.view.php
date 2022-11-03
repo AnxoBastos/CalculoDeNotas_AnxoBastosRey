ea6d3187ea78608fdf3f0dfdfd4b5f3bea2f9b48
anxo <anxo@anxo-VPCEB3A4E>
2022-11-03 17:55:52 +0100

    Commit Final (Tabla + tarjetas completadas)

--- views/calculo_notas.anxo_bastos_rey.view.php
+++ views/calculo_notas.anxo_bastos_rey.view.php
@@ -6,7 +6,113 @@
 <!-- Content Row -->
 
 <div class="row">
-    <!-- comment -->
+    <?php
+    if(isset($data['resultado'])){
+    ?>
+        <!-- TABLA -->
+        <div class="col-12">
+            <div class="card shadow mb-4">
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Resultados por módulo</h6>                                    
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Módulo</th>
+                                <th>Media</th>
+                                <th>Aprobados</th>
+                                <th>Suspensos</th>
+                                <th>Máximo</th>
+                                <th>Mínimo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach($data['resultado'] as $nombreModulo => $datosModulo){
+                                if($nombreModulo != 'listaSuspensos'){
+                            ?>
+                            <tr>
+                                <td><?php echo ucfirst($nombreModulo);?></td>
+                                <td><?php echo number_format($datosModulo['media'], 2, ',', '.');?></td>
+                                <td><?php echo $datosModulo['aprobados']; ?></td>
+                                <td><?php echo $datosModulo['suspensos']; ?></td>
+                                <td><?php echo $datosModulo['max']['nombre'].': '.number_format($datosModulo['max']['nota'], 2, ',', '.'); ?></td>
+                                <td><?php echo $datosModulo['min']['nombre'].': '.number_format($datosModulo['min']['nota'], 2, ',', '.'); ?></td>
+                            </tr>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    if(isset($data['resultado'])){
+    ?>
+    <!-- TARJETAS -->
+    <div class="col-12 col-lg-6">
+        <div class="alert alert-success">
+            <ol>
+            <?php 
+            foreach($data['resultado']['listaSuspensos'] as $nombre => $suspensas){
+                if($suspensas == 0){
+                    echo "<li>$nombre</li>";
+                }
+            }
+            ?>
+            </ol>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6">
+        <div class="alert alert-warning">
+            <ol>
+            <?php 
+            foreach($data['resultado']['listaSuspensos'] as $nombre => $suspensas){
+                if($suspensas >= 1){
+                    echo "<li>$nombre</li>";
+                }
+            }
+            ?>
+            </ol>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6">
+        <div class="alert alert-primary">
+            <ol>
+            <?php 
+            foreach($data['resultado']['listaSuspensos'] as $nombre => $suspensas){
+                if($suspensas <= 1){
+                    echo "<li>$nombre</li>";
+                }
+            }
+            ?>
+            </ol>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6">
+        <div class="alert alert-danger">
+            <ol>
+            <?php 
+            foreach($data['resultado']['listaSuspensos'] as $nombre => $suspensas){
+                if($suspensas >= 2){
+                    echo "<li>$nombre</li>";
+                }
+            }
+            ?>
+            </ol>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <!-- FORMULARIO -->
     <div class="col-12">
         <div class="card shadow mb-4">
             <div
@@ -16,7 +122,7 @@
             <!-- Card Body -->
             <div class="card-body">
                 <!--<form action="./?sec=formulario" method="post">                   -->
-                <form method="post" action="./?sec=iterativas08">
+                <form method="post" action="./?sec=calculo_notas.anxo_bastos_rey">
                     <!--<input type="hidden" name="sec" value="iterativas01" />-->
                     <div class="mb-3">
                         <label for="texto">Json Notas:</label>
